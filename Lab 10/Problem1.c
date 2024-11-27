9953f091c0947b5a78419a0dd0a1bb9b9421342c
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-27 17:15:55 +0500

    Recusrive program to add digits of number

---
+++ Lab 10/Problem1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int sumOfDigits(int n) {
+    // Base case: if the number is 0, return 0
+    if (n == 0)
+        return 0;
+    // Recursive case: add the last digit to the sum of the rest of the digits
+    return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    // Ensure the number is positive for simplicity
+    if (num < 0) {
+        num = -num;
+    }
+    
+    // Calculate and display the sum of digits
+    printf("The sum of digits is: %d\n", sumOfDigits(num));
+    
+    return 0;
+}
