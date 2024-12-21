3e17ef807b40bd410015dcb5e31a2551839cd2ed
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-12-21 16:03:51 +0500

    Update Q3_P.c

--- Lab # 9/Q3_P.c
+++ Lab # 9/Q3_P.c
@@ -1,29 +1,28 @@
 /*Implement a function that checks if a given integer is a prime number. Use this function in the
 main program to check if numbers entered by the user are prime.*/
 
-#include<stdio.h>
+#include <stdio.h>
 
-int isPrime(int num);
-int isPrime(int num){
-    //Numbers less than 2 are not prime
-    if(num<=1){
-        return 0; //Not prime
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0; // Not prime
     }
-    for(int i = 2; i*i < num; i++){
-        if(num % i == 0){
-            return 0; //Not prime
+    for (int i = 2; i * i <= num; i++) { // Fixed condition
+        if (num % i == 0) {
+            return 0; // Not prime
         }
     }
-    return 1; //The number is prime
+    return 1; // Prime
 }
-int main(){
+
+int main() {
     int number;
     printf("Input the number: ");
     scanf("%d", &number);
 
-    if(isPrime(number)){
+    if (isPrime(number)) {
         printf("The number %d is prime\n", number);
-    }else{
+    } else {
         printf("The number %d is not prime\n", number);
     }
 
